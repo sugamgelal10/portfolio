7fa0b84f0c3f93bbc5fdd284fe49603104ea15d0
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2024-03-23 16:01:50 +0545

    ui design completed

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const ProjectCard = ({img,no, title, desc}) => {
   return (
-    <div className='mb-[16px]'>
+    <div className='mx-[16px] mb-[16px]'>
         <div className='grid grid-cols-1 md:grid-cols-2 '>
             <div>
                 <img src={img} width={400} className='rounded-md'/>
             </div>
             <div className='flex flex-col justify-center'>
-                <div className='font-bold text-[28px]'>{no}</div>
-                <div className='font-extrabold text-[28px]'>{title}</div>
-                <div className='font-normal text-[16px] text-zinc-300'>{desc}</div>
+                <div className='text-[20px] font-bold md:text-[28px]'>{no}</div>
+                <div className=' text-[20px] font-extrabold md:text-[28px]'>{title}</div>
+                <div className='text-[12px] font-normal md:text-[16px] text-zinc-300'>{desc}</div>
             </div>
         </div>
     </div>
